3cc6b6e7d9bf57fce391cd87e06f3010065d96a6
darthbanana13 <radu.margineanu@gmail.com>
2023-11-07 16:30:11 +0100

    Port move lines shortcut

--- lua/revan/remap.lua
+++ lua/revan/remap.lua
@@ -9,3 +9,7 @@ vim.keymap.set({'n', 'v'}, '<Leader>l', '"0p', { remap = true })
 vim.keymap.set({'n', 'v'}, '<Leader>L', '"0P', { remap = true })
 vim.keymap.set({'n', 'v'}, '<Leader>y', '"+y', { remap = true })
 vim.keymap.set({'n', 'v'}, '<Leader>c', '"+d', { remap = true })
+
+-- Move lines easily with Shift+Up/Down
+vim.keymap.set({'n', 'i'}, '<S-Up>', ':m-2<CR>', { remap = true })
+vim.keymap.set({'n', 'i'}, '<S-Down>', ':m+<CR>', { remap = true })
