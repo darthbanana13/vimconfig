0e60a9c82d91428a283a4dde69d4299cf7b5ba81
darthbanana13 <radu.margineanu@gmail.com>
2024-02-25 16:54:26 +0100

    Fix leader timeout and smart search

--- lua/revan/settings.lua
+++ lua/revan/settings.lua
@@ -1,58 +1,66 @@
+local opt = vim.opt
+local api = vim.api
+
 -- Show relative line numbers to where the cursor is for easy jumping
-vim.o.number = true
-vim.o.relativenumber = true
+opt.number = true
+opt.relativenumber = true
 
 -- Customize line numbers, in insert mode use absolute, use relative everywhere else
-local numberToggleGroup = vim.api.nvim_create_augroup('numberToggle', { clear = true })
-vim.api.nvim_create_autocmd(
+local numberToggleGroup = api.nvim_create_augroup('numberToggle', { clear = true })
+api.nvim_create_autocmd(
   { 'BufEnter', 'FocusGained', 'InsertLeave' },
   {
     callback = function(ev)
-      vim.o.relativenumber = true
+      opt.relativenumber = true
     end,
     group = numberToggleGroup,
   }
 )
-vim.api.nvim_create_autocmd(
+api.nvim_create_autocmd(
   { 'BufLeave', 'FocusLost', 'InsertEnter' },
   {
     callback = function(ev)
-      vim.o.relativenumber = false
+      opt.relativenumber = false
     end,
     group = numberToggleGroup,
   }
 )
 
 -- Use spaces instead of tabs
-vim.o.expandtab = true
+opt.expandtab = true
 
 -- Reset the tab value
-vim.o.tabstop = 2
-vim.o.shiftwidth = 2
-vim.o.softtabstop = 2
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.softtabstop = 2
 
 -- Enable the mose everywhere
- vim.o.mouse = 'a'
+opt.mouse = 'a'
 
 -- Alaways store undos, even if the file is closed
-vim.o.undofile = true
+opt.undofile = true
 
 -- Automatically write the file when switching buffers.
-vim.o.autowriteall = true
+opt.autowriteall = true
 
 -- Live preview of search/replace
-vim.o.inccommand = 'split'
+opt.inccommand = 'split'
 
 -- Do case insensitive search unless a capital letter is added to the search term
-vim.o.smartcase = true
+opt.ignorecase = true
+opt.smartcase = true
+
+-- Always split to the right & bottom
+opt.splitright = true
+opt.splitbelow = true
 
 -- Set true/24-bit color
-vim.o.termguicolors = true
+opt.termguicolors = true
 
 -- Prevent the cursor going back one character when leaving insert
 local CursorColumnInsert = 1
 -- get the current position of the cursor and store it in the var above
-vim.api.nvim_create_autocmd(
+api.nvim_create_autocmd(
   { 'InsertEnter', 'CursorMovedI' },
   {
     callback = function(ev)
@@ -61,12 +69,12 @@ vim.api.nvim_create_autocmd(
   }
 )
 -- restore the cursor position when leaving insert
-vim.api.nvim_create_autocmd(
+api.nvim_create_autocmd(
   'InsertLeave',
   {
     callback = function(ev)
       if vim.fn.col('.') ~= CursorColumnInsert then
-        vim.api.nvim_win_set_cursor(0, { vim.fn.line('.'), vim.fn.col('.') })
+        api.nvim_win_set_cursor(0, { vim.fn.line('.'), vim.fn.col('.') })
       end
     end,
   }
