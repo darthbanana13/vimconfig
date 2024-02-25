178bc0836ff1632e66e84e27b0b1dd7e1298f4fc
darthbanana13 <radu.margineanu@gmail.com>
2024-02-25 16:54:26 +0100

    Fix leader timeout and smart search

--- lua/revan/remap.lua
+++ lua/revan/remap.lua
@@ -1,18 +1,21 @@
+local map = vim.keymap.set
+local remap = { noremap = true }
+
 -- Map the leader
 vim.g.mapleader = ','
 
 -- Easier interaction with the clipboard that does not mess the registers
-vim.keymap.set({'n', 'v'}, '<Leader>d', '"_d', { remap = true })
-vim.keymap.set({'n', 'v'}, '<Leader>p', '"+p', { remap = true })
-vim.keymap.set({'n', 'v'}, '<Leader>P', '"+P', { remap = true })
-vim.keymap.set({'n', 'v'}, '<Leader>l', '"0p', { remap = true })
-vim.keymap.set({'n', 'v'}, '<Leader>L', '"0P', { remap = true })
-vim.keymap.set({'n', 'v'}, '<Leader>y', '"+y', { remap = true })
-vim.keymap.set({'n', 'v'}, '<Leader>c', '"+d', { remap = true })
+map({'n', 'v'}, '<leader>d', '"_d', remap)
+map({'n', 'v'}, '<leader>p', '"+p', remap)
+map({'n', 'v'}, '<leader>P', '"+P', remap)
+map({'n', 'v'}, '<leader>l', '"0p', remap)
+map({'n', 'v'}, '<leader>L', '"0P', remap)
+map({'n', 'v'}, '<leader>y', '"+y', remap)
+map({'n', 'v'}, '<leader>c', '"+d', remap)
 
 -- Move lines easily with Shift+Up/Down
-vim.keymap.set({'n', 'i'}, '<S-Up>', ':m-2<CR>', { remap = true })
-vim.keymap.set({'n', 'i'}, '<S-Down>', ':m+<CR>', { remap = true })
+map({'n', 'i'}, '<S-Up>', ':m-2<CR>', remap)
+map({'n', 'i'}, '<S-Down>', ':m+<CR>', remap)
 
 -- Press // to search for the highlighted text
-vim.keymap.set('v', '//', [[y/\V<C-R>=escape(@",'/\')<CR><CR>]], { remap = true })
+map('v', '//', [[y/\V<C-R>=escape(@",'/\')<CR><CR>]], remap)
