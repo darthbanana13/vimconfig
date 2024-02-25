117babe940a0c220ad8b7994493cd06f4a6c363e
darthbanana13 <radu.margineanu@gmail.com>
2024-02-25 14:26:25 +0100

    Make new config act more like old config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,6 +4,7 @@ return {
   config = function()
     require('telescope').setup()
     local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+    vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+    vim.keymap.set('n', '<C-e>', builtin.buffers, {})
   end
 }
