afde9cfd43ab8dbd10be9e814629fcdb318071a1
darthbanana13 <radu.margineanu@gmail.com>
2024-02-26 21:45:19 +0100

    Add shortcut for ripgrep

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,8 +3,13 @@ return {
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
     require('telescope').setup()
+
     local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-    vim.keymap.set('n', '<C-e>', builtin.buffers, {})
+    local map = vim.keymap.set
+
+    map('n', '<C-p>', builtin.find_files, {})
+    map('n', '<C-e>', builtin.buffers, {})
+    map('n', '<leader>rg', builtin.grep_string, {})
+    map('n', '<leader>gr', builtin.live_grep, {})
   end
 }
