b6ebceffb68ec2664625fd8efa9e1cbf1974c78f
darthbanana13 <radu.margineanu@gmail.com>
2024-11-13 11:17:08 +0100

    Add more telescope mappings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,10 +6,15 @@ return {
 
     local builtin = require('telescope.builtin')
     local map = vim.keymap.set
+    local cmd = vim.api.nvim_create_user_command
 
     map('n', '<C-p>', builtin.find_files, {})
     map('n', '<C-e>', builtin.buffers, {})
-    map('n', '<leader>rg', builtin.grep_string, {})
-    map('n', '<leader>gr', builtin.live_grep, {})
+    map('n', '<leader>gr', builtin.grep_string, {})
+    map('n', '<leader>rg', builtin.live_grep, {})
+
+    cmd('Cmd', builtin.commands, {})
+    cmd('CmdH', builtin.command_history, {})
+    cmd('Reg', builtin.registers, {})
   end
 }
