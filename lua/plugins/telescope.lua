52d6b627aa8bb14a5d8f220129047da204a59f78
darthbanana13 <radu.margineanu@gmail.com>
2024-11-23 17:08:46 +0100

    Add description to telescope maps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,13 +8,13 @@ return {
     local map = vim.keymap.set
     local cmd = vim.api.nvim_create_user_command
 
-    map('n', '<C-p>', builtin.find_files, {})
-    map('n', '<C-e>', builtin.buffers, {})
-    map('n', '<leader>gr', builtin.grep_string, {})
-    map('n', '<leader>rg', builtin.live_grep, {})
+    map('n', '<C-p>', builtin.find_files, { desc = 'Find file name' })
+    map('n', '<C-e>', builtin.buffers, { desc = 'Filter open files' })
+    map('n', '<leader>gr', builtin.grep_string, { desc = 'Grep for string in unnamed register' })
+    map('n', '<leader>rg', builtin.live_grep, { desc = 'Live search for string' })
 
-    cmd('Cmd', builtin.commands, {})
-    cmd('CmdH', builtin.command_history, {})
-    cmd('Reg', builtin.registers, {})
-  end
+    cmd('Cmd', builtin.commands, { desc = 'Filter commands' })
+    cmd('CmdH', builtin.command_history, { desc = 'Filter command history' })
+    -- cmd('Reg', builtin.registers, { desc = 'Filter through registers' }) -- We have which-key for this, and yanky for history search
+  end,
 }
