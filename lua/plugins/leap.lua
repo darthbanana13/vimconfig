e0419415a55470906daddc1a7b2c9b42eb9dce5f
darthbanana13 <radu.margineanu@gmail.com>
2024-11-23 16:38:02 +0100

    Add descriptions to leap command

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -1,30 +1,39 @@
 return {
-  "ggandor/leap.nvim",
+  'ggandor/leap.nvim',
   dependencies = {
-    "tpope/vim-repeat",
-    "nvim-treesitter/nvim-treesitter",
+    'tpope/vim-repeat',
+    'nvim-treesitter/nvim-treesitter',
   },
   config = function()
     local map = vim.keymap.set
 
-    map('n',        's', '<Plug>(leap)')
-    map('n',        'S', '<Plug>(leap-from-window)')
-    map({'x', 'o'}, 's', '<Plug>(leap-forward)')
-    map({'x', 'o'}, 'S', '<Plug>(leap-backward)')
+    -- Replacement for native mappings
+    -- Normal mode:
+    -- s = cl (or xi)
+    -- S = CC
+    --
+    -- Visual mode:
+    -- s = c
+    -- S = Vc or c if already in linewise mode
+    -- For more :h leap-custom-mappings
+    map('n',        's', '<Plug>(leap)', { desc='Leap to next two typed letter location' })
+    map('n',        'S', '<Plug>(leap-from-window)', { desc='Leap to next other window\'s two type letter' })
+    map({'x', 'o'}, 's', '<Plug>(leap-forward)', { desc='Leap to the next group' })
+    map({'x', 'o'}, 'S', '<Plug>(leap-backward)', { desc='Leap to the previous group' })
 
-    local opts = require('leap').opts
+    local opt = require('leap').opts
     -- Define equivalence classes for brackets and quotes, in addition to
     -- the default whitespace group.
-    opts.equivalence_classes = { ' \t\r\n', '([{', ')]}', '\'"`' }
+    opt.equivalence_classes = { ' \t\r\n', '([{', ')]}', '\'"`' }
     -- Disable auto-jumping to the first match
-    opts.safe_labels = {}
+    opt.safe_labels = {}
 
     -- Use the traversal keys to repeat the previous motion without explicitly
     -- invoking Leap.
     require('leap.user').set_repeat_keys('<enter>', '<backspace>')
 
     map({'n', 'x', 'o'}, 'ga',  function ()
-      local sk = vim.deepcopy(require('leap').opts.special_keys)
+      local sk = vim.deepcopy(opt.special_keys)
       -- The items in `special_keys` can be both strings or tables - the
       -- shortest workaround might be the below one:
       sk.next_target = vim.fn.flatten(vim.list_extend({'a'}, {sk.next_target}))
@@ -32,10 +41,5 @@ return {
 
       require('leap.treesitter').select { opts = { special_keys = sk } }
     end)
-
-    -- Linewise.
-    map({'n', 'x', 'o'}, 'gA',
-      'V<cmd>lua require("leap.treesitter").select()<cr>'
-    )
   end,
 }
