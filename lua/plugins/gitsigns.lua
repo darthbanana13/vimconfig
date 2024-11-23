2ad5f7943f3e292fad96099bf018116484c2407d
darthbanana13 <radu.margineanu@gmail.com>
2024-11-23 10:53:30 +0100

    Change behaviour of git hints to line numbers

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,15 +1,16 @@
 return {
   'lewis6991/gitsigns.nvim',
-  config = function()
-    require('gitsigns').setup {
-      signs = {
-        add          = { text = '+' },
-        change       = { text = '~' },
-        delete       = { text = '_' },
-        topdelete    = { text = '‾' },
-        changedelete = { text = '~_' },
-        untracked    = { text = '┆' },
-      },
-    }
-  end,
+  opts = {
+    signs = {
+      add          = { text = '+' },
+      change       = { text = '~' },
+      delete       = { text = '_' },
+      topdelete    = { text = '‾' },
+      changedelete = { text = '~_' },
+      untracked    = { text = '┆' },
+    },
+    signcolumn = false,         -- Toggle with `:Gitsigns toggle_signs`
+    numhl      = true,          -- Toggle with `:Gitsigns toggle_numhl`
+    current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+  },
 }
