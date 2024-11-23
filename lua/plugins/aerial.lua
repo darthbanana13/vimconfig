4c217df89ce995acaf1aee0cf49f29891acf1478
darthbanana13 <radu.margineanu@gmail.com>
2024-11-23 17:04:16 +0100

    Add aerial to filter code symbols

---
+++ lua/plugins/aerial.lua
@@ -0,0 +1,22 @@
+return {
+  'stevearc/aerial.nvim',
+  dependencies = {
+     'nvim-treesitter/nvim-treesitter',
+     'nvim-tree/nvim-web-devicons',
+  },
+  config = function()
+    require("telescope").load_extension("aerial")
+    require('aerial').setup({
+      layout = {
+        default_direction = 'prefer_left',
+      },
+      close_automatic_events = { 'unfocus', 'switch_buffer', 'unsupported' },
+    })
+    vim.keymap.set(
+      'n',
+      '<C-s>',
+      require("telescope").extensions.aerial.aerial,
+      { desc='Filter buffer symbols' }
+    )
+  end,
+}
