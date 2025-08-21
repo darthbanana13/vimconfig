b78b96b9619c3cc82f8ba5e26ce7f2ec13e5b249
Radu Margineanu <radu.margineanu@gmail.com>
2025-08-21 23:00:02 +0200

    Customise todo

--- lua/plugins/todo.lua
+++ lua/plugins/todo.lua
@@ -6,6 +6,28 @@ return {
       { 'folke/trouble.nvim' },
       { 'nvim-telescope/telescope.nvim' },
     },
-    opts = {},
+    opts = {
+      keywords = {
+        FIX = { icon = " ", color = "error", alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, },
+        TODO = { icon = " ", color = "info", signs = false, },
+        HACK = { icon = " ", color = "warning", },
+        WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX", }, },
+        PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE", }, },
+        NOTE = { icon = " ", color = "hint", alt = { "INFO" }, signs = false, },
+        TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" }, },
+      },
+      colors = {
+        error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
+        warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
+        info = { "todo", "#2563EB" },
+        hint = { "DiagnosticHint", "#10B981" },
+        default = { "Identifier", "#7C3AED" },
+        test = { "Identifier", "#FF00FF" }
+      },
+      gui_style = {
+        fg = 'NONE',
+        bg = 'NONE',
+      },
+    },
   },
 }
