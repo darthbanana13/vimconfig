5e943c7298a6ee3818741ca5310a68f07e745eba
Radu Margineanu <radu.margineanu@gmail.com>
2026-01-29 12:36:03 +0100

    Fix: remove obvious default comment

--- lua/plugins/minuet-ai.lua
+++ lua/plugins/minuet-ai.lua
@@ -11,12 +11,7 @@ return {
         -- swapped in the GPUs memory, that can take a lot of time. If the
         -- model is loaded then responses are <1s
         request_timeout = 30,
-        n_completions = 1, -- recommend for local model for resource saving
-        -- I recommend beginning with a small context window size and incrementally
-        -- expanding it, depending on your local computing power. A context window
-        -- of 512, serves as an good starting point to estimate your computing
-        -- power. Once you have a reliable estimate of your local computing power,
-        -- you should adjust the context window to a larger value.
+        n_completions = 1,
         context_window = 32768,
         provider_options = {
           openai_fim_compatible = {
